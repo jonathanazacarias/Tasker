19fdc30e2726f2fb5dcf1700447c0856ec627e63
Jonathan Zacarias <jonathanazacarias@gmail.com>
2022-02-28 11:45:43 -0800

    Nodemon

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 const { ApolloServer, gql } = require('apollo-server');
 
+// dummy data
+const books = [
+    {
+        title: 'The Awakening',
+        author: 'Kate Chopin',
+    },
+    {
+        title: 'City of Glass',
+        author: 'Paul Auster',
+    },
+];
+
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
 // your data.
@@ -17,3 +29,20 @@ const typeDefs = gql`
     books: [Book]
   }
 `;
+
+// Resolvers define the technique for fetching the types defined in the
+// schema. This resolver retrieves books from the "books" array above.
+const resolvers = {
+    Query: {
+        books: () => books,
+    },
+};
+
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+});
